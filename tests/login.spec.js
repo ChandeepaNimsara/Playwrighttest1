401d1ab0a87c9458b2d49b5f76ade9a595a2bd52
ChandeepaNimsara <chandeeparodrigo97@gmail.com>
2025-05-22 10:54:18 +0530

    orange hrm login-out update1

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -13,16 +13,20 @@ test("Valid Login", async () => {
 
     const page = await context.newPage();
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    await page.getByPlaceholder("Username").type("Admin")
-    await page.getByPlaceholder("Password").type("admin123")
+    await page.getByPlaceholder("Username").fill("Admin")
+    await page.getByPlaceholder("Password").fill("admin123")
     await page.locator("button[type='submit']").click()
-    await page.waitForTimeout(2000)
+    await page.waitForTimeout(1800)
     await expect(page).toHaveURL(/.*dashboard\/index/)
 
-    await page.waitForTimeout(800)
+    await page.waitForTimeout(500)
+
     await page.locator("//img[@class='oxd-userdropdown-img']").click()
-    await page.waitForTimeout(2000)
-    await page.getByText("Logout").click
+    await page.waitForTimeout(1500)
+    await page.getByText("Logout").click()
+
+    await expect(page).toHaveURL(/.*auth\/login/)
+    await page.waitForTimeout(1000)
 
     await browser.close();
 });
